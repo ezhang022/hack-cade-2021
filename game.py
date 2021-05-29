49d67d0d8ce0062249bca842e0f78168d76c1d60
Kai Zheng <59148999+kaiz715@users.noreply.github.com>
2021-05-29 17:31:19 -0400

    thing

--- game.py
+++ game.py
@@ -1,18 +1,20 @@
 import pygame
 from pygame.locals import *
+import time
+import math
 
 pygame.init()
 
 screen_width = 1000
 screen_height = 1000
 
-screen = pygame.display.set_mode((screen_width,screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pacman Game')
 
-#define game variables
+# define game variables
 tile_size = 50
 
-#load images
+# load images
 bg_img = pygame.image.load('black.png')
 bg_img = pygame.transform.scale(bg_img, (screen_width, screen_height))
 
@@ -20,8 +22,8 @@ bg_img = pygame.transform.scale(bg_img, (screen_width, screen_height))
 class Player():
     def __init__(self, x, y):
         img = pygame.image.load('pacmanyellow.png')
-        self.image = pygame.transform.scale(img, (40,40))
-        
+        self.image = pygame.transform.scale(img, (50, 50))
+
         self.right = self.image
         self.left = pygame.transform.rotate(self.image, 180)
         self.up = pygame.transform.rotate(self.image, 90)
@@ -32,37 +34,142 @@ class Player():
         self.rect.y = y
         self.width = self.image.get_width()
         self.height = self.image.get_height()
-        self.direction = 1
+        self.prevdir = 0
+        self.turndir = None
+        self.cycles = 0
+
+    def legal_check(self, prevdir, direction):
+        global world_data
+        tile_x_pix = self.rect.x
+        tile_y_pix = self.rect.y
+        # if (direction == 1 and prevdir == 2) or (direction == 2 and prevdir == 1) or (direction == 0 and prevdir == 3):
+        #     tile_x_pos=math.floor(tile_x_pix/50)
+        #     tile_y_pos=math.floor(tile_y_pix/50)
+        # elif (direction == 0 and prevdir ==3) or(direction ==1 and prevdir ==3) or (direction == 2 and prevdir == 0):
+        #     tile_x_pos=math.ceil(tile_x_pix/50)
+        #     tile_y_pos=math.floor(tile_y_pix/50)
+        # else:
+        #0 left
+        #1 right
+        #2 up
+        #3 down
+
+        if (prevdir == 0 and direction == 3) or (prevdir == 3 and direction == 1) or (prevdir == 0 and direction == 2) or (prevdir == 3 and direction == 0) :
+            tile_x_pos=math.ceil(tile_x_pix/50)
+            tile_y_pos=math.floor(tile_y_pix/50)
+        elif(prevdir ==1 and direction ==3):
+            tile_x_pos=math.floor(tile_x_pix/50)
+            tile_y_pos=math.floor(tile_y_pix/50)
+        elif (prevdir ==2 and direction ==0):
+            tile_x_pos=math.ceil(tile_x_pix/50)
+            tile_y_pos=math.ceil(tile_y_pix/50)
+        else:
+            tile_x_pos=math.floor(tile_x_pix/50)
+            tile_y_pos=math.ceil(tile_y_pix/50)
+
+        if direction == 0:
+            tile_x_pos=round((tile_x_pix - 50)/50)
+        elif direction == 1:
+            tile_x_pos=round((tile_x_pix + 50)/50)
+        elif direction == 2:
+            tile_y_pos=round((tile_y_pix - 50)/50)
+        elif direction == 3:
+            tile_y_pos=round((tile_y_pix + 50)/50)
+        if world_data[tile_y_pos][tile_x_pos] == 1:
+            return False
+        return True
+
+    # def move_until(self, turn_dir):
+    #     if turn_dir ==0:
+    #         tile_x_pos = round(self.rect.x/50)+1
+    #         target_x = tile_x_pos*50
+    #         pixels_left = target_x - self.rect.x
+    #         cycles = pixels_left/5
+    #     elif turn_dir ==1:
+    #         tile_x_pos = round(self.rect.x/50)-1
+    #         target_x = tile_x_pos*50
+    #         pixels_left = target_x - self.rect.x
+    #         cycles = pixels_left/5
+    #     elif turn_dir ==2:
+    #         tile_y_pos = round(self.rect.y/50)-1
+    #         target_y = tile_y_pos*50
+    #         pixels_left = target_y - self.rect.y
+    #         cycles = pixels_left/5
+    #     elif turn_dir ==3:
+    #         tile_y_pos = round(self.rect.y/50)+1
+    #         target_y = tile_y_pos*50
+    #         pixels_left = target_y - self.rect.y
+    #         cycles = pixels_left/5
+    #     self.cycles = abs(cycles)
 
     def update(self):
-        
-        dx = 0
-        dy = 0
-        key = pygame.key.get_pressed()
-        if key[pygame.K_LEFT]:
+        dx=0
+        dy=0
+        key=pygame.key.get_pressed()
+
+        any_key_pressed=key[pygame.K_LEFT] or key[pygame.K_RIGHT] or key[pygame.K_UP] or key[pygame.K_DOWN]
+
+        if self.cycles > 0:
+            self.cycles -= 1
+            if self.turndir == 0:
+                dx=-5
+            elif self.turndir == 1:
+                dx=5
+            elif self.turndir == 2:
+                dy=-5
+            elif self.turndir == 3:
+                dy=5
+
+            self.rect.x += dx
+            self.rect.y += dy
+
+            screen.blit(self.image, self.rect)
+            return
+
+        if key[pygame.K_LEFT] and self.legal_check(self.prevdir, 0):
             dx -= 5
-            self.image = self.left
-        if key[pygame.K_RIGHT]:
+            self.image=self.left
+            self.prevdir=0
+        elif key[pygame.K_RIGHT] and self.legal_check(self.prevdir, 1):
             dx += 5
-            self.image = self.right
-        if key[pygame.K_UP]:
+            self.image=self.right
+            self.prevdir=1
+        elif key[pygame.K_UP] and self.legal_check(self.prevdir, 2):
             dy -= 5
-            self.image = self.up
-        if key[pygame.K_DOWN]:
+            self.image=self.up
+            self.prevdir=2
+        elif key[pygame.K_DOWN] and self.legal_check(self.prevdir, 3):
             dy += 5
-            self.image = self.down
-        
-        #check for collision
+            self.image=self.down
+            self.prevdir=3
+        else:
+            if self.prevdir == 0:
+                dx -= 5
+                self.image=self.left
+            elif self.prevdir == 1:
+                dx += 5
+                self.image=self.right
+            elif self.prevdir == 2:
+                dy -= 5
+                self.image=self.up
+            elif self.prevdir == 3:
+                dy += 5
+                self.image=self.down
+
+
+
+        # check for collision
         for tile in world.tile_list:
             if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
-                dx = 0
-            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
-                dy = 0
+                dx=0
 
-        #update player coordinates
+            elif tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                dy=0
+
+
+        # update player coordinates
         self.rect.x += dx
         self.rect.y += dy
-
 
         screen.blit(self.image, self.rect)
 
@@ -70,67 +177,68 @@ class Player():
 class World():
     def __init__(self, data):
 
-        self.tile_list = []
+        self.tile_list=[]
 
-        wall_img = pygame.image.load('blue.png')
-        
-        row_count = 0
+        wall_img=pygame.image.load('blue.png')
+
+        row_count=0
         for row in data:
-            col_count = 0
+            col_count=0
             for tile in row:
                 if tile == 1:
-                    img = pygame.transform.scale(wall_img, (tile_size, tile_size))
-                    img_rect = img.get_rect()
-                    img_rect.x = col_count * tile_size
-                    img_rect.y = row_count * tile_size
-                    tile = (img, img_rect)
+                    img=pygame.transform.scale(
+                        wall_img, (tile_size, tile_size))
+                    img_rect=img.get_rect()
+                    img_rect.x=col_count * tile_size
+                    img_rect.y=row_count * tile_size
+                    tile=(img, img_rect)
                     self.tile_list.append(tile)
                 col_count += 1
             row_count += 1
-    
+
     def draw(self):
         for tile in self.tile_list:
-            screen.blit(tile[0],tile[1])
-            
+            screen.blit(tile[0], tile[1])
 
-world_data = [
-[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-[1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1],
-[1,0,1,1,1,0,1,0,1,1,0,1,0,1,0,1,1,1,0,1],
-[1,0,0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0,0,1],
-[1,0,1,0,0,0,1,0,1,0,1,1,0,1,0,0,0,1,0,1],
-[1,0,1,0,1,0,1,0,0,0,0,0,0,1,0,1,0,1,0,1],
-[1,0,0,0,1,0,1,0,1,1,1,1,0,1,0,1,0,0,0,1],
-[1,1,1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1],
-[1,0,0,0,0,0,1,1,1,0,0,1,1,1,0,0,0,0,0,1],
-[1,0,1,1,1,0,1,0,0,0,0,0,0,1,0,1,1,1,0,1],
-[1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1],
-[1,0,1,0,0,0,1,0,1,0,0,1,0,1,0,0,0,1,0,1],
-[1,1,1,0,1,0,1,0,1,0,0,1,0,1,0,1,0,1,1,1],
-[1,0,0,0,1,0,1,0,1,1,1,1,0,1,0,1,0,0,0,1],
-[1,0,1,0,1,0,1,0,0,0,0,0,0,1,0,1,0,1,0,1],
-[1,0,1,0,0,0,1,0,1,0,1,1,0,1,0,0,0,1,0,1],
-[1,0,0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0,0,1],
-[1,0,1,1,1,0,1,0,1,1,0,1,0,1,0,1,1,1,0,1],
-[1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1],
-[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+
+world_data=[
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+[1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+[1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+[1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1],
+[1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1],
+[1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1],
+[1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1],
+[1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1],
+[1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1],
+[1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+[1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1],
+[1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1],
+[1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1],
+[1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1],
+[1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1],
+[1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+[1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+[1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
 
-world = World(world_data)
-player = Player(55,55)
-
-run = True
+world=World(world_data)
+player=Player(50, 50)
+clock=pygame.time.Clock()
+run=True
 while run:
-    
-    screen.blit(bg_img, (0,0))
+    clock.tick(60)
+    screen.blit(bg_img, (0, 0))
 
     world.draw()
     player.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
-    
+            run=False
+
     pygame.display.update()
 
 pygame.quit()
